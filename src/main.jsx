d092d05a324ca708aa54140821a2f1189533d4dd
sahil-programmer <sahiltarofawala312@gmail.com>
2024-10-12 17:25:50 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 
-export const server = "https://e-learning-server-1l0u.onrender.com";
+export const server = "https://localhost:5000";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <UserContextProvider>
